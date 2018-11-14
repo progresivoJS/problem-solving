3fdc0e21191f875596beb5da48251bcbdcb4be35
progresivojs <progresivojs@gmail.com>
2018-11-14 15:24:21 +0900

    feat: BOJ 1003

---
+++ boj/1003.py
@@ -0,0 +1,11 @@
+def add_tuple(a, b):
+    return a[0] + b[0], a[1] + b[1]
+
+if __name__ == '__main__':
+    test = int(input())
+    for i in range(test):
+        n = int(input())
+        d = [(1, 0), (0, 1)]
+        for j in range(2, n+1):
+            d.append(add_tuple(d[j-2], d[j-1]))
+        print("{} {}".format(d[n][0], d[n][1]))
